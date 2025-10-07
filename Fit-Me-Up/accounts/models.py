511fe2fac0ed6482ba84b2b600a2a1ab8ecb1701
YoonisKim <runheng1209@gmail.com>
2025-10-07 22:46:19 +0900

    FEAT: 회원가입/로그인 구현

--- Fit-Me-Up/accounts/models.py
+++ Fit-Me-Up/accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    pass
+
+    # 모델 함수
+    @staticmethod
+    def get_user_by_username(username):
+        try:
+            return User.objects.get(username=username)
+        except Exception:
+            return None
--- Fit-Me-Up/accounts/views.py
+++ Fit-Me-Up/accounts/views.py
@@ -1,3 +1,68 @@
-from django.shortcuts import render
+from rest_framework_simplejwt.serializers import RefreshToken
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import RegisterSerializer, AuthSerializer
+from rest_framework import status
 
-# Create your views here.
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+
+        # 유효성 검사 
+        if serializer.is_valid(raise_exception=True):
+            
+            # 유효성 검사 통과 후 객체 생성
+            user = serializer.save()
+
+            # user에게 refresh token 발급
+            token = RefreshToken.for_user(user)
+            refresh_token = str(token)
+            access_token = str(token.access_token)
+
+            res = Response(
+                {
+                    "user": serializer.data,
+                    "message": "register success!",
+                    "token": {
+                        "access_token": access_token,
+                        "refresh_token": refresh_token,
+                    }, 
+                },
+                status=status.HTTP_201_CREATED,
+            )
+            return res
+        
+
+class AuthView(APIView):
+    def post(self, request):
+        serializer = AuthSerializer(data=request.data)
+        
+        # 유효성 검사
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.validated_data['user']
+            access_token = serializer.validated_data['access_token']
+            refresh_token = serializer.validated_data['refresh_token']
+
+            res = Response(
+                {
+                    "user": {
+                        "id": user.id,
+                        "username": user.username,
+                        "email": user.email,
+                    },
+                    "message": "login success!",
+                    "token": {
+                        "access_token": access_token,
+                        "refresh_token": refresh_token,
+                    }, 
+                },
+                status=status.HTTP_200_OK,
+            )
+
+            res.set_cookie("access_token", access_token, httponly=True)
+            res.set_cookie("refresh_token", refresh_token, httponly=True)
+            return res
+        
+        # 유효성 검사 실패 시 오류 반환
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
